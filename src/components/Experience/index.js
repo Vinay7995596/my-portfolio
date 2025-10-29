3d447edb81f3b8b6754e010df8cebf36a9c07ef6
Vinay7995596 <ragichattuvinay7995@gmail.com>
2025-10-29 08:16:24 +0530

    change resume link and experience

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -78,7 +78,7 @@ const index = () => {
     return (
         <Container id="experience">
             <Wrapper>
-                <Title>Experience (Internship)</Title>
+                <Title>Experience</Title>
                 <Desc>
                     My work experience as a software engineer and working on different companies.
                 </Desc>
--- src/data/constants.js
+++ src/data/constants.js
@@ -14,7 +14,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Vinay7995596",
   resume:
-    "https://drive.google.com/file/d/1mCGnkAVH7SyvkY-Wmw2uZ0aO8rWDG5b2/view?usp=sharing",
+    "https://drive.google.com/file/d/1Y04NuBMsYpT9R_h8b0XHfPzp7kAfKaBZ/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ragi-chettu-vinay-3b062b244/",
 };
 
